66ce3b34218ddc94381bd10f576e067b261d003b
dwssmn <dapomy@gmail.com>
2020-12-08 16:02:22 -0500

    basic robot movement

--- 2021Robot/src/main/java/frc/robot/Robot.java
+++ 2021Robot/src/main/java/frc/robot/Robot.java
@@ -8,8 +8,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.*;
+import frc.robot.RobotMap;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,7 +26,8 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
+  public Gyro gyro;
+  public PID_Drive pid = new PID_Drive(1, 1, 1, gyro);
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -93,6 +97,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    
+    pid.executeTurn(20, 90);
   }
 
   /**
---
+++ 2021Robot/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class RobotMap {
+
+    public static final int FL = 4;
+    public static final int FR = 2;
+    public static final int BR = 1;
+    public static final int BL = 3;
+
+    public static final int TS = 5;
+    public static final int BS = 6;
+
+    public static final int Dc = 7;
+    public static final int Rc = 8;
+
+    public static final int Gatherer = 9;
+    public static final int Indexer = 10;
+    public static final int test = 100;
+
+    public static final int indexButton = 1;
+
+    public static final int pingChannel1 = 1;
+    public static final int pingChannel2 = 2;
+
+    public static final int echoChannel1 = 3;
+    public static final int echoChannel2 = 4;
+
+    //private static final int DriveController = 0;
+    //private static final int m_stick = 0;
+}
